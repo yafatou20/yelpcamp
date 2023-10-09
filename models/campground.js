fcde7dadf0510788b33db77ade82812f984b1bdb
yafatou20 <coulinour17@gmail.com>
2023-10-09 16:17:21 +0000

    first big commit

---
+++ models/campground.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title:String,
+    Image: String,
+    price:Number,
+    description:String,
+    location: String
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
